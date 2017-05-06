6d74d5f3662fe59d351b6d847c181b680c8755fc
Rovion <sidchristos_2@hotmail.com>
2017-05-06 10:30:35 +0300

    Adder init packages

--- app/src/main/java/com/example/threedots/findyourguy/Common/MainActivity.java
+++ app/src/main/java/com/example/threedots/findyourguy/Common/MainActivity.java
@@ -1,7 +1,9 @@
-package com.example.threedots.findyourguy;
+package com.example.threedots.findyourguy.Common;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.example.threedots.findyourguy.R;
 
 public class MainActivity extends AppCompatActivity {
 
---
+++ app/src/main/java/com/example/threedots/findyourguy/Model/Message.java
@@ -0,0 +1,8 @@
+package com.example.threedots.findyourguy.Model;
+
+/**
+ * Created by Stef on 06-May-17.
+ */
+
+public class Message {
+}
