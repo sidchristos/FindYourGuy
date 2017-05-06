030c20b01b38ac9ba285077fe3614c62326c364a
Evangelos <EvanPetr>
2017-05-06 11:50:15 +0300

    Εκανα το Room model

--- app/src/main/java/com/example/threedots/findyourguy/Model/Room.java
+++ app/src/main/java/com/example/threedots/findyourguy/Model/Room.java
@@ -5,4 +5,49 @@ package com.example.threedots.findyourguy.Model;
  */
 
 public class Room {
+    private long ID;
+    private String Title;
+    private long UIDCreator;
+    private boolean IsPrivate;
+
+    public Room(){}
+
+    public Room(long ID, String Title, long UIDCreator, boolean IsPrivate){
+        this.ID = ID;
+        this.Title = Title;
+        this.UIDCreator = UIDCreator;
+        this.IsPrivate = IsPrivate;
+    }
+
+    public long getID() {
+        return ID;
+    }
+
+    public long getUIDCreator() {
+        return UIDCreator;
+    }
+
+    public String getTitle() {
+        return Title;
+    }
+
+    public boolean getIsPrivate(){
+        return IsPrivate;
+    }
+
+    public void setID(long ID) {
+        this.ID = ID;
+    }
+
+    public void setUIDCreator(long UIDCreator) {
+        this.UIDCreator = UIDCreator;
+    }
+
+    public void setTitle(String title) {
+        this.Title = title;
+    }
+
+    public void setIsPrivate(boolean IsPrivate){
+        this.IsPrivate = IsPrivate;
+    }
 }
