94b4522019c88af0dc25777b2bf7d933950d1515
Evangelos <EvanPetr>
2017-05-06 12:06:34 +0300

    Εκανα το Message model

--- app/src/main/java/com/example/threedots/findyourguy/Model/Message.java
+++ app/src/main/java/com/example/threedots/findyourguy/Model/Message.java
@@ -5,4 +5,39 @@ package com.example.threedots.findyourguy.Model;
  */
 
 public class Message {
+    private long ID;
+    private long UIDSender;
+    private String Message;
+
+    public Message(){}
+
+    public Message(long ID, long  UIDSender, String Message){
+        this.ID = ID;
+        this.UIDSender = UIDSender;
+        this.Message = Message;
+    }
+
+    public void setID(long ID) {
+        this.ID = ID;
+    }
+
+    public void setUIDSender(long UIDSender) {
+        this.UIDSender = UIDSender;
+    }
+
+    public void setMessage(String message) {
+        this.Message = message;
+    }
+
+    public long getID() {
+        return ID;
+    }
+
+    public long getUIDSender() {
+        return UIDSender;
+    }
+
+    public String getMessage() {
+        return Message;
+    }
 }
